63adb4981ffa253896be9a2d519e435e319294df
PauliDuarte <anaduarte@s.ucom.edu.py>
2026-09-01 19:43:17 -0300

    Agregar log de conexion con Artemis

--- src/main/java/py/edu/ucom/is2/proyectocamel/config/IntegracionConfig.java
+++ src/main/java/py/edu/ucom/is2/proyectocamel/config/IntegracionConfig.java
@@ -6,6 +6,8 @@ import java.time.ZoneId;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.apache.camel.spi.IdempotentRepository;
 import org.apache.camel.support.processor.idempotent.MemoryIdempotentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +17,11 @@ import jakarta.jms.ConnectionFactory;
 @Configuration
 public class IntegracionConfig {
 
+    private static final Logger LOG = LoggerFactory.getLogger(IntegracionConfig.class);
+
     @Bean
     ConnectionFactory connectionFactory(@Value("${sipap.artemis.broker-url}") String brokerUrl) {
+        LOG.info("Configurando conexión Artemis con broker URL: {}", brokerUrl);
         return new ActiveMQConnectionFactory(brokerUrl);
     }
 
